713abafdc408d4d9db70bbd69f2dd17cdf842241
Eric Chen <ericc404@nycstudents.net>
2024-10-30 13:54:57 -0400

    Nothing

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let moviess = [
+  { name: "Inception", reviews: [2, 5, 2, 2] },
+  { name: "Jarvis", reviews: [] },
+];
+
+for (const movie of moviess) {
+  if (movie.reviews.length != 0) {
+    console.log();
+  }
+}
